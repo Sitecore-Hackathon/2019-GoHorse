554a0630c1503f7603f6185da735825a1957403f
Joao Neto <amancio.joao87@gmail.com>
2019-03-02 07:13:36 +0200

    Add remaining projects

---
+++ src/Feature/Login/code/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FaceLogin.Feature.Login
+{
+    public static class Constants
+    {
+        public struct Templates
+        {
+
+        }
+    }
+}
